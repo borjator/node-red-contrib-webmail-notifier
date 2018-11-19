719015fa3fff4a93e8de41b4dd4a7cb5c6ad1a3c
borjator <borja.tornero@nexiona.com>
2018-11-19 23:39:49 +0100

    Make the led device configurable

--- webmail-notifier.js
+++ webmail-notifier.js
@@ -8,21 +8,22 @@ module.exports = function(RED) {
 
     function WebmailNotifier(config) {
         RED.nodes.createNode(this, config)
+        this.ledDevice = config.leddev;
         var node = this;
-        const ledPath = "/sys/class/leds/"
+        const ledPath = "/sys/class/leds/" + this.ledDevice + ":";
 
         function switchLedError(err) {
             node.log("Error setting the led brightness: " + err);
         }
 
         function switchLedOn(led) {
-            fs.writeFile(ledPath + "riso_kagaku0:" + led + "/brightness", "1", switchLedError);
+            fs.writeFile(ledPath + led + "/brightness", "1", switchLedError);
         }
         
         function switchLedsOff() {
-            fs.writeFile(ledPath + "riso_kagaku0:red/brightness", "0", switchLedError);
-            fs.writeFile(ledPath + "riso_kagaku0:green/brightness", "0", switchLedError);
-            fs.writeFile(ledPath + "riso_kagaku0:blue/brightness", "0", switchLedError);
+            fs.writeFile(ledPath + "red/brightness", "0", switchLedError);
+            fs.writeFile(ledPath + "green/brightness", "0", switchLedError);
+            fs.writeFile(ledPath + "blue/brightness", "0", switchLedError);
         }
         
         node.on('input', function(msg) {
